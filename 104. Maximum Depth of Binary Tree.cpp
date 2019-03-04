13c2136c8aea58cd71371b6428c24f17f81c3272
xulzee <xulzee@163.com>
2019-03-04 18:36:47 +0800

    Create 104. Maximum Depth of Binary Tree.cpp

---
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,36 @@
+// @Time    : 2019/3/4 18:15
+// @Author  : xulzee
+// @Email   : xulzee@163.com
+// @Software: CLion
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include "utils.h"
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        left = maxDepth(root->left);
+        right = maxDepth(root-right);
+        return max(left, right) + 1;
+    }
+};
