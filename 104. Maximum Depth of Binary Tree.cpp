f5d28028c8d20216ba6701158b61a97361f1e5e6
xulzee <xulzee@163.com>
2019-03-06 14:26:29 +0800

    Update 104. Maximum Depth of Binary Tree.cpp

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -30,7 +30,7 @@ public:
         int left = 0;
         int right = 0;
         left = maxDepth(root->left);
-        right = maxDepth(root-right);
+        right = maxDepth(root->right);
         return max(left, right) + 1;
     }
 };
