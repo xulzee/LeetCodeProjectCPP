895fa3214064da9c39de58edf07e17c588a21892
xulzee <xulzee@163.com>
2019-03-17 14:35:37 +0800

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -43,6 +43,7 @@ public:
         }
         ListNode *t = NULL;
         t = reverseList(head->next);//先反转后面的链表，走到链表的末端结点
+
         head->next->next = head;//再将当前节点设置为后面节点的后续节点
         head->next = NULL;
 
