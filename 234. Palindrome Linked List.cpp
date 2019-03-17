668580e9e029787c0791984e155f468eb2b9ab16
xulzee <xulzee@163.com>
2019-03-17 14:35:55 +0800

    Update 234. Palindrome Linked List.cpp

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -18,12 +18,12 @@ struct ListNode {
     int val;
     ListNode *next;
 
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 class Solution {
 public:
-    ListNode *reverseList(ListNode *head) {
+    ListNode *reverseList(ListNode *head) {  //递归反转链表
         if (head == NULL || head->next == NULL) {
             return head;
         }
@@ -37,8 +37,8 @@ public:
 
     }
 
-    bool isPalindrome(ListNode *head) {
-        if (head == NULL or head->next == NULL){
+    bool isPalindrome1(ListNode *head) {
+        if (head == NULL or head->next == NULL) {
             return true;
         }
 
@@ -64,6 +64,52 @@ public:
             head = head->next;
         }
         return true;
+    }
+
+    bool isPalindrome(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) {
+            return true;
+        }
+        bool res = true;
+        //找到链表中间节点
+        ListNode *n1 = head;
+        ListNode *n2 = head;
+        while (n2->next != nullptr && n2->next->next != nullptr) {
+            n1 = n1->next;
+            n2 = n2->next->next;
+        }
+        // n2 表示下一半的前一个
+        // 反转 n2 ~ end
+        n2 = n1->next; // 中间节点的下一个
+        ListNode *n3;// 保存下一个节点
+        n1->next = nullptr; // mid -> nullptr
+        while (n2 != nullptr) {
+            n3 = n2->next;
+            n2->next = n1;
+            n1 = n2;
+            n2 = n3;
+        }
+        // n2 -> end node
+        n3 = n1; // save last node
+        n2 = head;
+        // check Palindrome
+        while (n1 != nullptr && n2 != nullptr) {
+            if (n1->val != n2->val) {
+                res = false;
+                break;
+            }
+            n1 = n1->next; // left to mid
+            n2 = n2->next; // right to mid
+        }
+        n1 = n3->next;
+        n3->next = nullptr;
+        while (n1 != nullptr) {
+            n2 = n1->next; // save next node
+            n1->next = n3;
+            n3 = n1;
+            n1 = n2;
+        }
+        return res;
     }
 };
 
@@ -94,14 +140,14 @@ vector<int> stringToIntegerVector(string input) {
     return output;
 }
 
-ListNode* stringToListNode(vector<int> l) {
+ListNode *stringToListNode(vector<int> l) {
     // Generate list from the input
     vector<int> list = l;
 
     // Now convert that list into linked list
-    ListNode* dummyRoot = new ListNode(0);
-    ListNode* ptr = dummyRoot;
-    for(int item : list) {
+    ListNode *dummyRoot = new ListNode(0);
+    ListNode *ptr = dummyRoot;
+    for (int item : list) {
         ptr->next = new ListNode(item);
         ptr = ptr->next;
     }
@@ -115,8 +161,8 @@ string boolToString(bool input) {
 }
 
 int main() {
-    vector<int> A = {1,0,1};
-    ListNode* head = stringToListNode(A);
+    vector<int> A = {1, 2, 3, 2, 1};
+    ListNode *head = stringToListNode(A);
 
     bool ret = Solution().isPalindrome(head);
 
