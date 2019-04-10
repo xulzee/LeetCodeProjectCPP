80757ad63a99cc00b5211e9e1734b0f511c7f7cb
xulzee <xulzee@163.com>
2019-04-10 14:17:24 +0800

    Update 79. Word Search.cpp

--- source/79. Word Search.cpp
+++ source/79. Word Search.cpp
@@ -4,6 +4,8 @@
 // @Software: CLion
 #include <string>
 #include <vector>
+#include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -56,13 +58,13 @@ public:
         return cur->end != 0;
     }
 
-    bool startWith(const string &prefix){
-        if (prefix.empty()){
+    bool startWith(const string &prefix) {
+        if (prefix.empty()) {
             return false;
         }
         TrieNode *cur = root;
         for (const auto &item : prefix) {
-            if (cur->next[item - 'a'] == nullptr){
+            if (cur->next[item - 'a'] == nullptr) {
                 return false;
             }
             cur = cur->next[item - 'a'];
@@ -73,7 +75,49 @@ public:
 
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
+    bool exist(vector<vector<char>> &board, string word) {
+        if (board.empty() || word.empty()) {
+            return false;
+        }
+        bool res = false;
+        for (int i = 0; i < board.size(); ++i) { // row
+            for (int j = 0; j < board[0].size(); ++j) { // column
+                if (board[i][j] == word[0]) {
+                    res = res || dfs(board, word, 0, j, i);
+                    if (res) {
+                        return res;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 
+    bool dfs(vector<vector<char>> &board, string &word, int wordPosition, int column,
+             int row) { // 回溯
+        if (wordPosition == word.size()) {
+            return true;
+        }
+        if (column < 0 || column > board[0].size() - 1 || row < 0 || row > board.size() - 1 ||
+            board[row][column] == '#') {
+            return false;
+        }
+        bool ret = false;
+        if (board[row][column] == word[wordPosition]) {
+            char cur = word[wordPosition];
+            board[row][column] = '#';
+            ret = ret || dfs(board, word, wordPosition + 1, column, row + 1)
+                  || dfs(board, word, wordPosition + 1, column, row - 1)
+                  || dfs(board, word, wordPosition + 1, column + 1, row)
+                  || dfs(board, word, wordPosition + 1, column - 1, row);
+            board[row][column] = cur;
+        }
+        return ret;
     }
 };
+
+int main() {
+    string test = "ab";
+    vector<vector<char>> board = {{'a'}};
+    cout << Solution().exist(board, test) << endl;
+}
