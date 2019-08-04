f03406404d0e4d498f3cc49666279af148f09499
liuze.xlz <liuze.xlz@alibaba-inc.com>
2019-08-05 00:10:46 +0800

    Update 102. Binary Tree Level Order Traversal.cpp

--- source/autumn-recruitment/102. Binary Tree Level Order Traversal.cpp
+++ source/autumn-recruitment/102. Binary Tree Level Order Traversal.cpp
@@ -5,7 +5,7 @@
 
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode *root) {
+    vector<vector<int>> levelOrder1(TreeNode *root) {
         vector<vector<int>> res;
         if (root == nullptr) {
             return res;
@@ -30,4 +30,31 @@ public:
         }
         return res;
     }
+
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        if (root == nullptr) {
+            return res;
+        }
+
+        helper(root, 0);
+        return res;
+    }
+
+    void helper(TreeNode *root, int level) {
+        if (res.size() == level) {
+            res.emplace_back();
+        }
+
+        res[level].push_back(root->val);
+
+        if (root->left != nullptr) {
+            helper(root->left, level + 1);
+        }
+        if (root->right != nullptr) {
+            helper(root->right, level + 1);
+        }
+    }
+
+private:
+    vector<vector<int>> res;
 };
