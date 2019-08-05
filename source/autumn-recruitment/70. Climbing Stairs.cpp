0471282a65e9e97219db4f04c7323680e8f32451
liuze.xlz <liuze.xlz@alibaba-inc.com>
2019-08-05 20:38:08 +0800

    Create 70. Climbing Stairs.cpp

---
+++ source/autumn-recruitment/70. Climbing Stairs.cpp
@@ -0,0 +1,31 @@
+//
+// Created by liuze.xlz on 2019-08-05.
+//
+#include "utils.h"
+
+class Solution {
+public:
+    int process(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return process(n - 1) + process(n - 2);
+    }
+
+    int dynamicProcess(int n) {
+        vector<int> dp(n + 1, 1);
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        return dynamicProcess(n);
+    }
+};
+
+int main(){
+    cout  << Solution().climbStairs(3) << endl;
+}
