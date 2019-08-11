ce0dbc509562678313dfb75e960482258ad07acf
liuze.xlz <liuze.xlz@alibaba-inc.com>
2019-08-12 00:32:45 +0800

    Create 412. Fizz Buzz.cpp

---
+++ source/autumn-recruitment/412. Fizz Buzz.cpp
@@ -0,0 +1,23 @@
+//
+// Created by liuze.xlz on 2019-08-12.
+//
+#include "utils.h"
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i = 1; i <= n; ++i) {
+            if (i % 15 == 0) {
+                res.emplace_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.emplace_back("Fizz");
+            } else if (i % 5 == 0) {
+                res.emplace_back("Buzz");
+            } else {
+                res.emplace_back(to_string(i));
+            }
+        }
+        return res;
+    }
+};
