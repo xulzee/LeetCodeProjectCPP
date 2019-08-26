0efdb0d03ac029622269eb70a7f615519823ee9b
liuze.xlz <liuze.xlz@alibaba-inc.com>
2019-08-26 11:04:01 +0800

    Update 46. Permutations.cpp

--- source/autumn-recruitment/46. Permutations.cpp
+++ source/autumn-recruitment/46. Permutations.cpp
@@ -7,34 +7,47 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int> &nums) {
-        vector<int> temp;
-        vector<int> visited(nums.size(), 0);
-        vector<vector<int>> res;
-        dfs(nums, visited, temp, res, 0);
-        return res;
+        vector<int> cur_nums;
+        visited_.assign(nums.size(), 0);
+        backtrack(nums, 0);
+        return res_;
     }
 
 
-    void dfs(vector<int> &nums, vector<int> &visited, vector<int> &temp,vector<vector<int>> &res, int len) {
-        if (len >= 2) {
-            res.push_back(temp);
+    void backtrack1(vector<int> &nums, vector<int> &cur_nums, int len) {
+        if (len >= nums.size()) {
+            res_.push_back(cur_nums);
             return;
         }
 
         for (int j = 0; j < nums.size(); ++j) {
-            if (visited[j] == 0){
-                visited[j] = 1;
-                temp.push_back(nums[j]);
-                dfs(nums, visited, temp, res, len + 1);
-                temp.pop_back();
-                visited[j] = 0;
+            if (visited_[j] == 0) {
+                visited_[j] = 1;
+                cur_nums.push_back(nums[j]);
+                backtrack1(nums, cur_nums, len + 1);
+                cur_nums.pop_back();
+                visited_[j] = 0;
             }
         }
     }
+
+    void backtrack(vector<int> &nums, int i) {
+        if (i == nums.size())
+            res_.push_back(nums);
+        for (int j = i; j < nums.size(); j++) {
+            swap(nums[i], nums[j]);
+            backtrack(nums, i + 1);
+            swap(nums[i], nums[j]);
+        }
+    }
+
+private:
+    vector<vector<int>> res_;
+    vector<int> visited_;
 };
 
-int main(){
-    vector<int> nums = {1,2,3};
+int main() {
+    vector<int> nums = {1, 2, 3};
     Solution().permute(nums);
 }
 
