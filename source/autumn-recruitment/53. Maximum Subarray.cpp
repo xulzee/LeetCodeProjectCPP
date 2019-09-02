8beb193582880e203de56fde09a503423bd1e33a
liuze.xlz <liuze.xlz@alibaba-inc.com>
2019-09-02 16:32:04 +0800

    Update 53. Maximum Subarray.cpp

--- source/autumn-recruitment/53. Maximum Subarray.cpp
+++ source/autumn-recruitment/53. Maximum Subarray.cpp
@@ -2,21 +2,51 @@
 // Created by liuze.xlz on 2019-08-06.
 //
 #include "utils.h"
+#include <tuple>
 
 class Solution {
 public:
     int maxSubArray(vector<int> &nums) {
-        int res = nums[0];
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int res = nums.front();
         int sum = 0;
-        for (int num : nums) {
-            if (sum > 0){
+        for (auto &num : nums) {
+            if (sum > 0) {
                 sum += num;
-            } else{
+            } else {
                 sum = num;
             }
             res = max(res, sum);
         }
         return res;
+    }
+
+    int maxSubArrayWithIndex(vector<int> &nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        tuple<int, int, int> res = {nums[0], 0, 0}; // num, start, end
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (sum > 0) {
+                sum += nums[i];
+                ++end;
+            } else {
+                sum = nums[i];
+                start = i;
+                end = i;
+            }
+
+            if (sum > get<0>(res)) {
+                res = make_tuple(sum, start, end);
+            }
+        }
+        return get<0>(res);
     }
 };
 
