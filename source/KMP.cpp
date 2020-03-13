4529068bfa4568b4a652f2621c85b99f7b79020f
xulzee <xulzee@163.com>
2020-03-13 18:46:33 +0800

    Update KMP.cpp

--- source/KMP.cpp
+++ source/KMP.cpp
@@ -51,13 +51,100 @@ int getIndexOf(const string &s, const string &t) {
 }
 
 
+class Test {
+    int a;
+};
+
+typedef struct _data {
+    int id;
+    char name[6];
+    long long time;
+    char add[4];
+} data;
+
+#include <queue>
+
+class Solution {
+public:
+    void swap(vector<int> &arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    int findKthLargest(vector<int> &nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> q;
+        for (auto num:nums) {
+            if (q.size() == k) {
+                if (num <= q.top()) {
+                    continue;
+                }
+                q.pop();
+            }
+            q.push(num);
+        }
+        return q.top();
+    }
+
+    int _findKthLargest(vector<int> &nums, int k) {
+        vector<int> ret;
+        k = nums.size() - k;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            ret = partition(nums, left, right);
+            if (k < ret[0]) {
+                right = ret[0] - 1;
+            } else if (k > ret[1]) {
+                left = ret[1] + 1;
+            } else {
+                return nums[k];
+            }
+        }
+        return nums[k];
+    }
+
+    vector<int> partition(vector<int> &arr, int left, int right) {
+        int less = left - 1;
+        int more = right;
+        while (left < more) {
+            if (arr[left] < arr[right]) {
+                swap(arr, left, less + 1);
+                ++left;
+                ++less;
+            } else if (arr[left] == arr[right]) {
+                ++left;
+            } else {
+                swap(arr, left, more - 1);
+                --more;
+            }
+        }
+        swap(arr, left, right);
+        return {less + 1, left};
+    }
+};
+
 int main() {
-    string s = "123qweabababab123";
-    string test = "abababab";
-    auto ret = getIndexOf(s, test);
-    char c[4][8] = {{"ab"},
-                    {"bc"},
-                    {"cd"}};
+    vector<int> test = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+    auto ans = Solution()._findKthLargest(test, 4);
+    cout << ans << endl;
+
+
+
+//    auto x = sizeof(data);
+//    char arr[] = {'a', 'b','\0', 'c','d'};
+//    auto y = sizeof(arr );
+//    string s = "123qweabababab123";
+//    string a = "a";
+//    auto z = a.substr(0, 1);
+//    string test = "abcaabc";
+//    auto ret = getIndexOf(s, test);
+//    char c[4][8] = {{"ab"},
+//                    {"bc"},
+//                    {"cd"}};
 
 }
 
